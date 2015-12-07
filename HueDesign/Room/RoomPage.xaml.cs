2166ab7fbdf58169beab3862dd553f35e2a88cdf
Jannick van Ballegooijen <jannickvb@msn.com>
2015-12-07 22:52:46 +0100

    started room dev

--- HueDesign/Room/RoomPage.xaml.cs
+++ HueDesign/Room/RoomPage.xaml.cs
@@ -22,9 +22,36 @@ namespace HueDesign.Room
     /// </summary>
     public sealed partial class RoomPage : Page
     {
+        Point scrollMousePoint = new Point();
+        double hOff = 1;
+        double vOff = 1;
+        bool isActive = false;
+
         public RoomPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ScrollViewer_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            scrollViewer.CapturePointer(e.Pointer);
+            scrollMousePoint = e.GetCurrentPoint(scrollViewer).Position;
+            hOff = scrollViewer.HorizontalOffset;
+            vOff = scrollViewer.VerticalOffset;
+
+            isActive = true;
+        }
+
+        private void ScrollViewer_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (isActive)
+                scrollViewer.ChangeView(hOff + (scrollMousePoint.X - e.GetCurrentPoint(scrollViewer).Position.X), vOff + (scrollMousePoint.Y - e.GetCurrentPoint(scrollViewer).Position.Y), scrollViewer.ZoomFactor);
+        }
+
+        private void ScrollViewer_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            scrollViewer.ReleasePointerCaptures();
+            isActive = false;
         }
     }
 }
